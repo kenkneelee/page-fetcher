0e40b462e6e53d3266570e9867793169c061f8cd
Kenny Lee <kenny.lee927@gmail.com>
2023-08-13 16:39:59 -0400

    add stretch, invalid url and path handling

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,42 @@
 const request = require("request");
 const fs = require("fs");
+const readline = require("readline");
 // Store command line arguments
 const args = process.argv.slice(2);
+const URL = args[0];
+const path = args[1];
+// Setup user input listening
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-request(args[0], (error, response, body) => {
+request(URL, (error, response, body) => {
   // Print the error if one occurred
   if (error) {
     console.log("error:", error);
   }
-
-  // Print the response status code if a response was received
-  console.log("statusCode:", response && response.statusCode);
-
-  // Write the file to given path
-  fs.writeFile(args[1], body, (err) => {
+  console.log(response.statusCode);
+  if (response.statusCode === 404) {
+    console.log("Invalid URL!");
+    process.exit();
+  }
+  fs.access(path, fs.F_OK, (err) => {
     if (err) {
-      console.log(err);
+      fs.writeFile(path, body, (err) => {
+        if (err) {
+          if (err.errno === -2) {
+            console.log("Invalid path!");
+          }
+          process.exit();
+        }
+        console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+      });
+    } else {
+      console.log("File already exists! Overwrite? Y/N");
+      process.exit();
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
+    // return;
+    // Write the file to given path
   });
 });
